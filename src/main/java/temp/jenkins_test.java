4e703a79a4914725f0ea64b72f489a4a41aa97a5
Nagarajan Yadav <nagarajan.chelliya@gmail.com>
2019-07-14 14:05:43 +0530

    hello world project files added

---
+++ src/main/java/temp/jenkins_test.java
@@ -0,0 +1,16 @@
+package temp;
+
+/**
+ * Hello world!
+ *
+ */
+public class jenkins_test 
+{
+    public static void main( String[] args )
+    {
+    	
+    	for(int i=0;i<10;i++) {
+        System.out.println( "Hello World!" );
+    	}
+    	}
+}
